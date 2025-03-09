835fba5d190e4fec577b3d8fb01b96d2c164c449
Mathilde Morell <morell.mathilde74@gmail.com>
2025-03-09 19:03:34 +0100

    fix : deployment issue

--- app/clouds.tsx
+++ app/clouds.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function Clouds() {
   return (
@@ -8,10 +9,12 @@ export default function Clouds() {
       initial={{ y: -100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 2, ease: "easeOut" }}
-      className="absolute bottom-[-300px] w-full border-none cursor-pointer flex justify-center items-center z-10 flex-col"
+      className="absolute lg:-bottom-[500px] sm:-bottom-[550px] w-full border-none cursor-pointer flex justify-center items-center z-10 flex-col"
     >
-      <img src="/clouds.svg" alt="Nuages" className="w-full" />
-      <h1 className="p-0 m-0 text-[#F2E3DE] text-4xl font-bold">Je t'ai eu !</h1>
+      <Image src="/clouds.svg" alt="Nuages" width={500} height={300} className="w-full sm:translate-y-0" />
+      <h1 className="p-0 m-0 text-[#F2E3DE] text-4xl font-bold hidden sm:block">
+        Je t&apos;ai eu !
+      </h1>
     </motion.div>
   );
 }
--- app/projects/clouds.tsx
+++ app/projects/clouds.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function Clouds() {
   return (
@@ -10,8 +11,10 @@ export default function Clouds() {
       transition={{ duration: 2, ease: "easeOut" }}
       className="absolute lg:-bottom-[500px] sm:-bottom-[550px] w-full border-none cursor-pointer flex justify-center items-center z-10 flex-col"
     >
-      <img src="/clouds.svg" alt="Nuages" className="w-full sm:translate-y-0" />
-      <h1 className="p-0 m-0 text-[#F2E3DE] text-4xl font-bold hidden sm:block">Je t'ai eu encore une fois!</h1>
+      <Image src="/clouds.svg" alt="Nuages" width={500} height={300} className="w-full sm:translate-y-0" />
+      <h1 className="p-0 m-0 text-[#F2E3DE] text-4xl font-bold hidden sm:block">
+        Je t&apos;ai eu encore une fois!
+      </h1>
     </motion.div>
   );
 }
